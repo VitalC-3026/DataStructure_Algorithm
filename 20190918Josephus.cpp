8e6d984bffdcac54fb2ee83dbf1001d53228e168
VitalC-3026 <50989602+VitalC-3026@users.noreply.github.com>
2020-03-27 17:12:17 +0800

    Stack and List

---
+++ 20190918Josephus.cpp
@@ -0,0 +1,117 @@
+﻿#include <iostream>
+
+
+using namespace std;
+
+class Node {
+	int status;
+	int original;
+	Node* next;
+	Node() {
+		status = -1;
+		original = -1;
+		next = nullptr;
+
+	}
+	Node(int i, Node* n) {
+		status = i;
+		original = i;
+		next = n;
+	}
+	friend class CircularList;
+	
+};
+
+class CircularList 
+{
+private:
+	Node* head;
+	int number_Node;
+	int kill_Node;
+
+public:
+	CircularList(int number, int kill){
+		this->kill_Node = kill;
+		this->number_Node = number;
+		int k = 1;
+		head = new Node(-1, nullptr);
+		head->next = new Node(k++, nullptr);
+		Node* current = head->next;
+		while (k <= number_Node - 1) {
+			current->next = new Node(k++, nullptr);
+			current = current->next;
+		}
+		if (k == number_Node) 
+		{
+			current->next = new Node(k, head->next);
+		}
+	}
+	int getNumber_Node() { return number_Node; }
+	int getKill_Node() { return kill_Node; }
+	void delete_Node();
+	void reset_number(Node* tmp);
+	void printInfo();
+};
+
+void CircularList::delete_Node()
+{
+	Node* tmp, * current = head->next;
+	while (current->next->status < kill_Node && current->status < current->next->status) 
+	{
+		current = current->next;
+	}
+	if (current->next->status == kill_Node) 
+	{
+		tmp = current->next;
+		current->next = current->next->next;
+		head = current->next;
+		number_Node--;
+		reset_number(tmp);
+		//printInfo();
+	}
+	else if (current->next->status < current->status) 
+	{
+		cout << "Game over!" << endl;
+		//printInfo();
+		return;
+	}
+}
+
+void CircularList::reset_number(Node* tmp) 
+{
+	Node* current = tmp->next;
+	int k = 1;
+	current->status = k++;
+	current = current->next;
+	while (current != tmp->next) 
+	{
+		current->status = k++;
+		current = current->next;
+	}
+}
+
+void CircularList::printInfo() 
+{
+	Node* current = head->next;
+	cout << "kill:" << kill_Node << "total:" << number_Node << endl;
+	while (current->next != head->next) 
+	{
+		cout << "original:" << current->original << "\tpresent:" << current->status << endl;
+		current = current->next;
+	}
+	cout << "original:" << current->original << "\tpresent:" << current->status << endl;
+}
+
+int main()
+{
+	CircularList l(9,3);
+	int kill = l.getKill_Node();
+	int total = l.getNumber_Node();
+	while (total >= kill) 
+	{
+		l.delete_Node();
+		total--;
+		l.printInfo();
+	}
+}
+
---
+++ 20190922StackSort.cpp
@@ -0,0 +1,210 @@
+﻿//#include <iostream>
+//#include <ctime>
+//#include <fstream>
+//using namespace std;
+//
+//struct Dataset
+//{
+//	long long mainkey;
+//	int file_No;
+//	char division;
+//	string province;
+//	int int_number;
+//	double double_number;
+//	string word;
+//
+//	Dataset()
+//	{
+//		mainkey = 0;
+//		file_No = 0;
+//		division = 0;
+//		int_number = 0;
+//		double_number = 0;
+//	}
+//	void printInfo()
+//	{
+//		cout << mainkey << '\t' << file_No << '\t' << division << '\t' << province << '\t';
+//		cout << int_number << '\t' << double_number << '\t' << word << endl;
+//	}
+//};
+//
+//template<typename T>
+//class Stack 
+//{
+//private:
+//	T* stack;
+//	int MAXSIZE;
+//	int top;
+//public:
+//	Stack(int maxsize) {
+//		MAXSIZE = maxsize;
+//		stack = new T[MAXSIZE + 1];
+//		top = 0;
+//	}
+//	bool isEmpty() {
+//		if (top == 0)
+//			return true;
+//		else
+//			return false;
+//	}
+//	bool isFull() {
+//		if (top == MAXSIZE)
+//			return true;
+//		else
+//			return false;
+//	}
+//	void push(T);
+//	T pop();
+//	T gettop() {
+//		if (isEmpty())
+//			cout << "The stack is empty!" << endl;
+//		else
+//			return stack[top - 1];
+//	}
+//};
+//template<>
+//Dataset Stack<Dataset>::gettop() {
+//	if (isEmpty())
+//		cout << "The stack is empty!" << endl;
+//	else
+//		return stack[top - 1];
+//}
+//
+//template <typename T>
+//void Stack<T>::push(T item) 
+//{
+//	if (!isFull())
+//		stack[top++] = item;
+//	else
+//		cout << "The stack is full!" << endl;
+//}
+//
+//template <typename T>
+//T Stack<T>::pop()
+//{
+//	if (!isEmpty())
+//		return stack[--top];
+//	else
+//		cout << "The stack is empty!" << endl;
+//}
+//
+//
+//
+//template <typename T>
+//T* StackSort2(T* sorting, int len) 
+//{
+//	Stack<T> first(len + 1), second(len + 1);
+//	int k = 0, count = 0, t = 0;
+//	T* sorted = new T[len];
+//	while (k <= len - 1) 
+//	{
+//		first.push(sorting[k++]);
+//	}
+//	T temp;
+//	temp = first.pop();
+//	second.push(temp);
+//	bool second_isEmpty = false;
+//	while (!first.isEmpty()) 
+//	{
+//		temp = first.pop();
+//		if (second.gettop() >= temp) {
+//			second.push(temp);
+//		}
+//		else 
+//		{
+//			while (temp > second.gettop()) 
+//			{
+//				first.push(second.pop());
+//				count++;
+//				if (second.isEmpty()) 
+//				{
+//					second_isEmpty = true;
+//					break;
+//				}
+//			}
+//			second.push(temp);
+//			while (t < count) 
+//			{
+//				second.push(first.pop());
+//				t++;
+//			}
+//			count = 0;
+//		}
+//	}
+//	k = 0;
+//	while (!second.isEmpty()) 
+//	{
+//		sorted[k++] = second.pop();
+//	}
+//	return sorted;
+//}
+//
+//template<> 
+//Dataset* StackSort2<Dataset>(Dataset* sorting, int len) 
+//{
+//	Stack<Dataset> first(len + 1), second(len + 1);
+//	int k = 0, count = 0, t = 0;
+//	Dataset* sorted = new Dataset[len];
+//	while (k <= len - 1)
+//	{
+//		first.push(sorting[k++]);
+//	}
+//	Dataset temp;
+//	temp = first.pop();
+//	second.push(temp);
+//	bool second_isEmpty = false;
+//	while (!first.isEmpty())
+//	{
+//		temp = first.pop();
+//		if (second.gettop().int_number >= temp.int_number) {
+//			second.push(temp);
+//		}
+//		else
+//		{
+//			while (temp.int_number > second.gettop().int_number)
+//			{
+//				first.push(second.pop());
+//				count++;
+//				if (second.isEmpty())
+//				{
+//					second_isEmpty = true;
+//					break;
+//				}
+//			}
+//			second.push(temp);
+//			while (t < count)
+//			{
+//				second.push(first.pop());
+//				t++;
+//			}
+//			count = 0;
+//		}
+//	}
+//	k = 0;
+//	while (!second.isEmpty())
+//	{
+//		sorted[k++] = second.pop();
+//	}
+//	return sorted;
+//}
+//
+//int main()
+//{
+//	int* a = new int[10];
+//	srand((unsigned)int(time));
+//	Dataset* test = new Dataset[10];
+//	Dataset* sorted = new Dataset[10];
+//	ifstream infile("E:\\C++\\DataSet\\4.txt");
+//	for (int i = 0; i < 10; i++) 
+//	{
+//		infile >> test[i].mainkey >> test[i].file_No >> test[i].division >> test[i].province >> test[i].int_number >> test[i].double_number >> test[i].word;
+//		test[i].printInfo();
+//	}
+//	cout << endl;
+//	sorted = StackSort2<Dataset>(test, 10);
+//	for (int i = 0; i < 10; i++)
+//	{
+//		sorted[i].printInfo();
+//	}
+//}
+//
